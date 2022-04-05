0d51dd621bd1ef04dfd16cc1b2bf1f128b41fae5
fhasan <fhasan@mmhayes.com>
2022-04-05 14:09:59 -0400

    Deleted all. will create new

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,5 @@
 public class Calculator {
   
-  public static double add(double a, double b){
-  return a+b;}
-  
-  public static void main(String[] args){
 
-   double c =  add(4,5);
-   System.out.println(c);
-  }
 }
 
