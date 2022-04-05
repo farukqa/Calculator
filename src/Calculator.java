5aec0196349b74d4a88713aa16e2d4c863b7ae9a
fhasan <fhasan@mmhayes.com>
2022-04-05 09:30:05 -0400

    added the static keyword

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,11 @@
 public class Calculator {
   
-  public double add(double a, double b){
+  public static double add(double a, double b){
   return a+b;}
   
   public static void main(String[] args){
+
+    add(4,5);
   }
 }
 
